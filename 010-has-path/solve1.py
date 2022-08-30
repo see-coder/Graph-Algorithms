111853d62b5c58ab1364cdcc58fc734f62506050
see-coder <imrakesh.cse@gmail.com>
2022-08-30 17:37:27 +0530

    has-path implemented using depth first

---
+++ 010-has-path/solve1.py
@@ -0,0 +1,28 @@
+# depth first solution
+def has_path(graph, src, dst):
+    if src == dst:
+        return True
+    
+    for neighbor in graph[src]:
+        if has_path(graph, neighbor, dst) == True:
+            return True
+    
+    return False
+
+
+graph = {
+    'f': ['g', 'i'],
+    'g': ['h'],
+    'h': [],
+    'i': ['g', 'k'],
+    'j': ['i'],
+    'k': []
+}
+
+print(has_path(graph, 'f', 'k')) # True
+print(has_path(graph, 'f', 'j')) # False
+
+# n = number of nodes
+# e = number of edges
+# Time: O(e)
+# Space: O(n)
