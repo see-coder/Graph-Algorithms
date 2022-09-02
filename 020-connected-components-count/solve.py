b76ff5f84d6600b4dc1489900ed1cec733b67fe3
see-coder <imrakesh.cse@gmail.com>
2022-09-02 23:20:24 +0530

    connected-components-count implemented

---
+++ 020-connected-components-count/solve.py
@@ -0,0 +1,71 @@
+# depth first solution
+def connected_components_count(graph):
+    visited = set()
+    count = 0
+    
+    for node in graph:
+        if explore(graph, node, visited) == True:
+            count += 1
+    
+    return count
+
+def explore(graph, current, visited):
+    if current in visited:
+        return False
+    
+    visited.add(current)
+    
+    for neighbor in graph[current]:
+        explore(graph, neighbor, visited)
+    
+    return True
+
+
+# Testing
+print(connected_components_count({
+    0: [8, 1, 5],
+    1: [0],
+    5: [0, 8],
+    8: [0, 5],
+    2: [3, 4],
+    3: [2, 4],
+    4: [3, 2]
+})) # -> 2
+
+print(connected_components_count({
+    1: [2],
+    2: [1,8],
+    6: [7],
+    9: [8],
+    7: [6, 8],
+    8: [9, 7, 2]
+})) # -> 1
+
+print(connected_components_count({
+    3: [],
+    4: [6],
+    6: [4, 5, 7, 8],
+    8: [6],
+    7: [6],
+    5: [6],
+    1: [2],
+    2: [1]
+})) # -> 3
+
+print(connected_components_count({})) # -> 0
+
+print(connected_components_count({
+    0: [4,7],
+    1: [],
+    2: [],
+    3: [6],
+    4: [0],
+    6: [3],
+    7: [0],
+    8: []
+})) # -> 5
+
+# n = number of nodes
+# e = number of edges
+# Time: O(e)
+# Space: O(n)
