2146fc76e4d5dfe7db90086943fcaf2512e187cd
see-coder <imrakesh.cse@gmail.com>
2022-09-03 16:18:11 +0530

    island-count implemented using depth first

---
+++ 035-island-count/solve.py
@@ -0,0 +1,48 @@
+# depth first solution
+def island_count(grid):
+    visited = set()
+    count = 0
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            if explore(grid, r, c, visited) == True:
+                count += 1
+    return count
+
+def explore(grid, r, c, visited):
+    row_inbounds = 0 <= r < len(grid)
+    col_inbounds = 0 <= c < len(grid[0])
+    if not row_inbounds or not col_inbounds:
+        return False
+    
+    if grid[r][c] == 'W':
+        return False
+    
+    pos = (r, c)
+    if pos in visited:
+        return False
+    visited.add(pos)
+    
+    explore(grid, r - 1, c, visited)
+    explore(grid, r + 1, c, visited)  
+    explore(grid, r, c - 1, visited)
+    explore(grid, r, c + 1, visited)
+    
+    return True
+
+
+# Testing
+grid = [
+    ['W', 'L', 'W', 'W', 'W'],
+    ['W', 'L', 'W', 'W', 'W'],
+    ['W', 'W', 'W', 'L', 'W'],
+    ['W', 'W', 'L', 'L', 'W'],
+    ['L', 'W', 'W', 'L', 'L'],
+    ['L', 'L', 'W', 'W', 'W'],
+]
+
+print(island_count(grid)) # -> 3
+
+# r = number of rows
+# c = number of columns
+# Time: O(rc)
+# Space: O(rc)
